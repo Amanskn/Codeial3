7dd9d744c8f66aaf0f5c8d72bbe31219f59a3567
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-20 14:12:39 +0530

    Set up the view engine

---
+++ controllers/users_controller.js
@@ -0,0 +1,6 @@
+module.exports.home=function(req,res){
+    res.send('<h1>User Home Page</h1>')
+}
+module.exports.profile=function(req,res){
+    res.send('<h1>User profile</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ const homeController=require('../controllers/home_controller');
 console.log("Router loaded");
 
 
-router.get('/',homeController.home)
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
 
 module.exports=router;
