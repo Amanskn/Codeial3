8a91f9713a16aba3e55bb916a3c5df50c7775f50
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-24 23:36:26 +0530

    Done with checking the authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,21 @@
 const express=require('express');
+const passport = require('passport');
 
 const router=express.Router();
 
 
 const postsController=require('../controllers/posts_controller');
 
-router.post('/create',postsController.create);
+
+// Now here we use the middleware to restrict someone from being able to make a post
+// via creating a form using developer tools without establishing his/her identity. 
+// So now he/she would not be able to make a post
+// on our website even if he/she will create a form in developer tools and try to fiddle with our website, Got it:)
+
+// CONCLUSION : we are putting a check at the url's action level, i.e. we are not letting the user to
+// penetrate into my action without passing the check of authentication. So if the user is not signed in 
+//  he/she will not be able to reach the action of creating a post. So an identity will always be established before making a post. 
+
+router.post( '/create' , passport.checkAuthentication , postsController.create );
 
 module.exports=router;
