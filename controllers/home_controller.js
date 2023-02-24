b81abc5c24403270ce965eaedd53add8ab6a9f88
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-24 20:58:41 +0530

    Done with displaying posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,43 @@
+const Post=require('../models/post');
+
+
+
+
+
 module.exports.home=function(req,res){
 
     // console.log(req.cookies);
     // res.cookie("user_iddd","12344455")
-    return  res.render('home',{
-        title:"Home_Page"
+
+    // Post.find({},function(err,posts){
+    //     if(err){
+    //         console.log("Error in fetching all the posts from the database");
+    //         return;
+    //     }
+    //     return  res.render('home',{
+    //         title:"Home_Page",
+    //         posts:posts
+    //     });
+    // })
+
+
+    // populating the user of each post excluding the password field
+    // and also sorting in reverse chronological order
+    Post.find({}).populate('user',"-password")
+    .sort({"createdAt":-1})
+    // below will also work
+    // .sort("-createdAt")
+    .exec(function(err,posts){
+        if(err){
+            console.log("Error in fetching all the posts from the database");
+            return;
+        }
+        return  res.render('home',{
+            title:"Home_Page",
+            posts:posts   
+        });
+        
+
     });
+    
 }
