580c7476d3ccb746883d50d6409601f94a8c1727
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-25 14:25:41 +0530

    implemented the functionality of deleting a comment (authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -30,3 +30,42 @@ module.exports.create=function(req,res){
 
 }
 
+
+
+module.exports.destroy=function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(err){
+            console.log("Error in fetching the comment to be deleted from the database",err);
+            return;
+        }
+        else if(comment){
+
+            if(comment.user==req.user.id){
+
+                let postId=comment.post;
+                comment.remove();
+
+                Post.findByIdAndUpdate(postId,{ $pull : {comments:req.params.id}},function(err,post){
+                    if(err){
+                        console.log("Error in updating the post while deleting a comment",err);
+                        return;
+                    }
+                    return res.redirect('back');
+
+                });
+
+
+
+            }
+            else{
+                console.log("You are unauthorized to delete this comment");
+                return res.redirect('back');
+            }
+        }
+        else{
+            console.log("The comment to be deleted is not found in the databse");
+            return;
+        }
+    })
+}
+
