ce8370ea11a3b52cc644099a36626615985cb7e6
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-25 21:14:28 +0530

    converted homecontroller into async/await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -24,55 +24,39 @@ module.exports.home = async function(req,res){
 
     // populating the user of each post excluding the password field
     // and also sorting in reverse chronological order
-    Post.find({})
-    .populate('user',"-password")
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user',
-            // to hide as many fields as I want from being populated
-            // select:'-name -password -email',
 
-            // to only populate the name field of the user
-            select:'name',
-            
-            
-        },
-        
-        options: {
-            sort: { createdAt: -1 } // sort by createdAt field in descending order
-        }
-        
-        
-    })
-    .sort({"createdAt":-1})
-    // below will also work
-    // .sort("-createdAt")
-    .exec(function(err,posts){
-        if(err){
-            console.log("Error in fetching all the posts from the database");
-            return;
-        }
 
-        User.find({},function(err,users){
-            if(err){
-                console.log("Error in finding all the users from the database");
-                return;
+    try{
+
+        let posts= await Post.find({})
+        .populate('user',"-password")
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user',
+                // to hide as many fields as I want from being populated
+                // select:'-name -password -email',
+                // to only populate the name field of the user
+                select:'name',
+            },
+            options: {
+                sort: { createdAt: -1 } // sort by createdAt field in descending order
             }
-            else{
+        })
+        // below will also work
+        // .sort("-createdAt")
+        .sort({"createdAt":-1});
 
-                return  res.render('home',{
-                    title:"Codeial | Home",
-                    posts:posts,
-                    all_users:users   
-                });
-            }
+        let users = await User.find({});
+
+        return  res.render('home',{
+            title:"Codeial | Home",
+            posts:posts,
+            all_users:users   
         });
-
-
-        
-        
-
-    });
-    
+    }
+    catch(err){
+        console.log("Error",err);
+        return;
+    }
 }
