aa2dc28a1f87add233ce6c2e0f77bb07e46aba81
Aman Madd <29maddhesiaaman@gmail.com>
2023-02-28 22:58:08 +0530

    Done with Sending HTML Template Emails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -6,14 +6,14 @@ const nodeMailer=require('../config/nodemailer');
 exports.newComment=(comment)=>{
     // console.log("Inside newComment mailer");
 
-    // let htmlString=nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
+    let htmlString=nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from:'amandev1938@gmail.com',
         to:comment.user.email,
         subject:'New Comment Published!!',
-        html:'<h1>Yes, your comment is now published!!!</h1>'
-        // html:htmlString
+        // html:'<h1>Yes, your comment is now published!!!</h1>'
+        html:htmlString
     },(err,info)=>{
         if(err){
             console.log('Error in sending mail',err);
